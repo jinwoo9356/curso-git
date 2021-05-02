14a1149ab328db4b91b748eb62c036041a9b8d93
Hugo da Costa <hugo_costa_brasil@hotmail.com>
2021-05-02 20:56:46 -0300

    Projeto iniciado mostrando as funções do exercicio

---
+++ EmployeeExercice/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeExercice
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public double Salary { get; private set; }
+
+        public Employee(string name, int id, double salary)
+        {
+            Name = name;
+            Id = id;
+            Salary = salary;
+        }
+
+        public double IncreaseSalary(double percentage)
+        {
+            return Salary += Salary * (percentage / 100);
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Salary; 
+        }
+    }
+}
---
+++ EmployeeExercice/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeExercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("How many employees will be registred? ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+
+                Console.WriteLine("Employee #{0}: ", (i + 1));
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                int salary = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                list.Add(new Employee(name, id, salary));
+                Console.WriteLine();
+            }
+            Console.Write("Enter the employee id that will have salary increased: ");
+            int ide = int.Parse(Console.ReadLine());
+
+
+            if (list.Find(x => x.Id == ide) != null)
+            {
+                Console.Write("Enter the percentage: ");
+                int percentage = int.Parse(Console.ReadLine());
+                list.Find(x => x.Id == ide).IncreaseSalary(percentage);
+            }
+            else
+                Console.WriteLine("!ID Does not exist!");
+
+            Console.WriteLine("Updated list of employees:");
+            foreach (Employee item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
