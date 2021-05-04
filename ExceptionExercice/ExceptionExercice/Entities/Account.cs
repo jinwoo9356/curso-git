960211125649b124345f3df63f170bf097daf421
jinwoo9356 <hugo_costa_brasil@hotmail.com>
2021-05-03 21:01:33 -0300

    Exercicio de Exceptions finalizado

---
+++ ExceptionExercice/ExceptionExercice/Entities/Account.cs
@@ -0,0 +1,37 @@
+﻿using ExceptionExercice.Entities.Exceptions;
+namespace ExceptionExercice.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double deposit)
+        {
+            Balance = deposit;
+        }
+
+        public void Withdraw(double withdraw)
+        {
+            if (withdraw > WithdrawLimit)
+            {
+                throw new DomainException("The amaout exceeds withdraw limit");
+            }
+            if (withdraw > Balance)
+            {
+                throw new DomainException("Not enough balance");
+            }
+            Balance -= withdraw;
+        }
+    }
+}
---
+++ ExceptionExercice/ExceptionExercice/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ExceptionExercice.Entities.Exceptions;
+using ExceptionExercice.Entities;
+
+
+namespace ExceptionExercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data: ");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine());
+
+                Console.Write("Withdraw limit: ");
+                double withdraw = double.Parse(Console.ReadLine());
+
+                Account account = new Account(number, holder, balance, withdraw);
+
+                Console.WriteLine("\nEnter amout for withdraw: ");
+                account.Withdraw(double.Parse(Console.ReadLine()));
+                Console.WriteLine("New balance: " + account.Balance);
+
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw Error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected Error: " + e.Message);
+            }
+        }
+    }
+}
